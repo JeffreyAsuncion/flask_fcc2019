3a7a1241d62d8e76f0159c57f5c0de2f07969111
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-08-01 22:12:23 -0400

    get and post methods

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+# use this to get input from url
 @app.route('/home/users/<string:name>/posts/<int:id>')
 def hello(name, id):
     return "Hello, " + name + " Your id is :" + str(id)
+
+# use to onlyget
+@app.route('/onlyget', methods=['GET', 'POST'])
+def get_req():
+    return 'You can only get this webpage.'
+
 
 if __name__ == "__main__":
     app.run(debug=True)

a06ca855253a8c05b7a1cb0c250076f042677f93
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-08-01 22:49:28 -0400

    base setup in templates

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Home Page</h1>"
+    return render_template('index.html')
 
 
 # use this to get input from url
